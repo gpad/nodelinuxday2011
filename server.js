6b224844a613e3798cd504f46e89d98a8c231f0e
GPad <gpadovani@gmail.com>
2011-10-12 13:21:12 +0000

    changed the host and port for heroku

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var http = require('http');
+var util = require('util');
 
-var host = '0.0.0.0';
-var port = process.env.C9_PORT;
+var host = '127.0.0.1';
+var port = 80;
+
+if (process.env.C9_PORT) {
+    host = '0.0.0.0';
+    port = process.env.C9_PORT;
+}
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Linux Day!!!\n');
+  res.end('Hello Linux Day!!!\n' + util.inspect(process.env));
 }).listen(port, host);
 console.log('Server running at http://' + host + ':' + port + '/');

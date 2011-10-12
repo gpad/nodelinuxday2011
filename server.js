7db9f08fb0455516e9c633065b83695ccfcf8853
GPad <gpadovani@gmail.com>
2011-10-12 12:23:31 +0000

    aggiunte le librerie di development

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var http = require('http');
 var host = '0.0.0.0';
 var port = process.env.C9_PORT;
 
-
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Linux Day!!!\n');

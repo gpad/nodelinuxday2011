0bc7cf4b41dcef48ba58eb176eb288346b8621e0
GPad <gpadovani@gmail.com>
2011-10-12 16:31:40 +0000

    little modify

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ if (process.env.C9_PORT) {
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Linux Day!!!\n' + util.inspect(process.env));
+  res.end('Hello Linux Day!!!\n');
 }).listen(port, host);
 console.log('Server running at http://' + host + ':' + port + '/');

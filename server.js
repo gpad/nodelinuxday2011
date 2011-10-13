c28531e3c63dae6adade56f342d1d82ca7bef74a
GPad <gpadovani@gmail.com>
2011-10-13 12:24:08 +0000

    sistemato il problema di sincronia

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 var http = require('http');
 var util = require('util');
 var fs = require('fs');
-
 var host = '0.0.0.0';
 var port = process.env.PORT || 3000;
-
 if (process.env.C9_PORT) {
     host = '0.0.0.0';
     port = process.env.C9_PORT;
 }
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  
-  fs.readFile('index.html', function(err, data) {
-      if (err) throw err;
-      res.write(data);
-  });
-  
-  res.end('');
+http.createServer(function(req, res) {
+    
+    console.log(util.inspect(req));
+    
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('index.html', function(err, data) {
+        if (err) throw err;
+        res.write(data);
+        res.end('');
+    });
 }).listen(port, host);
 console.log('Server running at http://' + host + ':' + port + '/');
-

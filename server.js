c3de3f47edc662bc14d64b5c2556c0f85095d238
GPad <gpadovani@gmail.com>
2011-10-13 12:37:58 +0000

    gestione di formidale

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var util = require('util');
 var fs = require('fs');
+var formidable = require('formidable');
+
 var host = '0.0.0.0';
 var port = process.env.PORT || 3000;
 if (process.env.C9_PORT) {
@@ -11,13 +13,42 @@ http.createServer(function(req, res) {
     
     console.log(util.inspect(req));
     
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('index.html', function(err, data) {
-        if (err) throw err;
-        res.write(data);
-        res.end('');
-    });
+    if (req.url == '/') {
+        fs.readFile('index.html', function(err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            if (err) throw err;
+            res.write(data);
+            res.end('');
+        });
+    }
+    else if (req.url == '/upload') {
+        var form = new formidable.IncomingForm(),
+          files = [],
+          fields = [];
+        form.uploadDir = '.';
+        form.keepExtensions = true;
+        form.on('fileBegin', function(name, file) {
+            //console.log("**************");
+            //console.log(name);
+            //console.log(util.inspect(file));
+            file.path = file.name;
+            //console.log("**************");
+        }).on('field', function(field, value) {
+            console.log(field, value);
+            fields.push([field, value]);
+        }).on('file', function(field, file) {
+            console.log(field, file);
+            files.push([field, file]);
+        }).on('end', function() {
+            console.log('-> upload done');
+            res.writeHead(200, {
+                'content-type': 'text/plain'
+            });
+            res.write('received fields:\n\n ' + util.inspect(fields));
+            res.write('\n\n');
+            res.end('received files:\n\n ' + util.inspect(files));
+        });
+        form.parse(req);
+    }
 }).listen(port, host);
 console.log('Server running at http://' + host + ':' + port + '/');
